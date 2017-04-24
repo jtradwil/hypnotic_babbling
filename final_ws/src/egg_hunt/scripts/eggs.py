1e91303277af55ff44972161e5a5eda5efdc4f39
jtradwil <jtradwil@mtu.edu>
2017-04-24 10:23:45 -0400

    Tuned egg counter.

--- final_ws/src/egg_hunt/scripts/eggs.py
+++ final_ws/src/egg_hunt/scripts/eggs.py
@@ -7,14 +7,17 @@ pinkLower = np.array([165, 89, 102], np.uint8)
 pinkUpper = np.array([180, 204, 255], np.uint8)
 yellowLower = np.array([22, 102, 153], np.uint8)
 yellowUpper = np.array([33, 204, 255], np.uint8)
-greenLower = np.array([75, 127, 38], np.uint8)
+greenLower = np.array([60, 127, 38], np.uint8)
 greenUpper = np.array([95, 255, 255], np.uint8)
 blueLower = np.array([100, 127, 153], np.uint8)
 blueUpper = np.array([113, 255, 255], np.uint8)
 orangeLower = np.array([9, 127, 127], np.uint8)
-orangeUpper = np.array([15, 255, 255], np.uint8)
-purpleLower = np.array([120, 127, 127], np.uint8)
-purpleUpper = np.array([128, 179, 255], np.uint8)
+orangeUpper = np.array([20, 255, 255], np.uint8)
+purpleLower = np.array([120, 77, 127], np.uint8)
+purpleUpper = np.array([165, 179, 255], np.uint8)
+
+erode=3
+dilate=12
 
 def crop_height(image):
     egg_x=0
@@ -33,16 +36,66 @@ def crop_height(image):
     for cnt in contours:	
         x,y,w,h = cv2.boundingRect(cnt)	#fit a square around the target
         if (w*h>egg_w*egg_h):
-	    egg_x=x
+            egg_x=x
             egg_y=y
             egg_w=w
             egg_h=h
     if (egg_h>0):
         cropped=image[egg_y-float(0.2*egg_h):egg_y+egg_h+float(0.2*egg_h), 0:width]
     else:
-	cropped=image
+	    cropped=image
+
     return cropped
 
+def crop_width(image):
+    egg_x=0
+    egg_y=0
+    egg_w=0
+    egg_h=0
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    height, width, channels = image.shape
+
+    maskPink = cv2.inRange(hsv, pinkLower, pinkUpper)
+    #maskPink = cv2.erode(maskPink, None, iterations=erode)
+    maskPink = cv2.dilate(maskPink, None, iterations=dilate)
+    
+    maskYellow = cv2.inRange(hsv, yellowLower, yellowUpper)
+    #maskYellow = cv2.erode(maskYellow, None, iterations=erode)
+    maskYellow = cv2.dilate(maskYellow, None, iterations=dilate)
+    
+    maskGreen = cv2.inRange(hsv, greenLower, greenUpper)
+    #maskGreen = cv2.erode(maskGreen, None, iterations=erode)
+    maskGreen = cv2.dilate(maskGreen, None, iterations=dilate)
+
+    maskBlue = cv2.inRange(hsv, blueLower, blueUpper)
+    #maskBlue = cv2.erode(maskBlue, None, iterations=erode)
+    maskBlue = cv2.dilate(maskBlue, None, iterations=dilate)
+
+    maskOrange = cv2.inRange(hsv, orangeLower, orangeUpper)
+    #maskOrange = cv2.erode(maskOrange, None, iterations=erode)
+    maskOrange = cv2.dilate(maskOrange, None, iterations=dilate)
+
+    maskPurple = cv2.inRange(hsv, purpleLower, purpleUpper)
+    #maskPurple = cv2.erode(maskPurple, None, iterations=erode)
+    maskPurple = cv2.dilate(maskPurple, None, iterations=dilate)
+
+    mask=maskPink+maskYellow+maskGreen+maskBlue+maskOrange+maskPurple
+    masked = cv2.bitwise_and(image, image, mask=mask)
+    contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:	
+        x,y,w,h = cv2.boundingRect(cnt)	#fit a square around the target
+        if (w*h>egg_w*egg_h):
+            egg_x=x
+            egg_y=y
+            egg_w=w
+            egg_h=h
+    if (egg_h>0):
+        cropped=masked[0:height, egg_x-float(0.2*egg_w):egg_x+egg_w+float(0.2*egg_h)]
+    else:
+	    cropped=image
+
+    return cropped   
+    
 def avg_size(image):
 
     avg_size=0
@@ -50,33 +103,33 @@ def avg_size(image):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     maskPink = cv2.inRange(hsv, pinkLower, pinkUpper)
-    maskPink = cv2.erode(maskPink, None, iterations=2)
-    maskPink = cv2.dilate(maskPink, None, iterations=12)
+    maskPink = cv2.erode(maskPink, None, iterations=erode)
+    maskPink = cv2.dilate(maskPink, None, iterations=dilate)
     contoursPink, hierarchy = cv2.findContours(maskPink,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
     maskYellow = cv2.inRange(hsv, yellowLower, yellowUpper)
-    maskYellow = cv2.erode(maskYellow, None, iterations=2)
-    maskYellow = cv2.dilate(maskYellow, None, iterations=12)
+    maskYellow = cv2.erode(maskYellow, None, iterations=erode)
+    maskYellow = cv2.dilate(maskYellow, None, iterations=dilate)
     contoursYellow, hierarchy = cv2.findContours(maskYellow,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
     maskGreen = cv2.inRange(hsv, greenLower, greenUpper)
-    maskGreen = cv2.erode(maskGreen, None, iterations=2)
-    maskGreen = cv2.dilate(maskGreen, None, iterations=12)
+    maskGreen = cv2.erode(maskGreen, None, iterations=erode)
+    maskGreen = cv2.dilate(maskGreen, None, iterations=dilate)
     contoursGreen, hierarchy = cv2.findContours(maskGreen,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
     maskBlue = cv2.inRange(hsv, blueLower, blueUpper)
-    maskBlue = cv2.erode(maskBlue, None, iterations=2)
-    maskBlue = cv2.dilate(maskBlue, None, iterations=12)
+    maskBlue = cv2.erode(maskBlue, None, iterations=erode)
+    maskBlue = cv2.dilate(maskBlue, None, iterations=dilate)
     contoursBlue, hierarchy = cv2.findContours(maskBlue,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
     maskOrange = cv2.inRange(hsv, orangeLower, orangeUpper)
-    maskOrange = cv2.erode(maskOrange, None, iterations=2)
-    maskOrange = cv2.dilate(maskOrange, None, iterations=12)
+    maskOrange = cv2.erode(maskOrange, None, iterations=erode)
+    maskOrange = cv2.dilate(maskOrange, None, iterations=dilate)
     contoursOrange, hierarchy = cv2.findContours(maskOrange,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
     maskPurple = cv2.inRange(hsv, purpleLower, purpleUpper)
-    maskPurple = cv2.erode(maskPurple, None, iterations=2)
-    maskPurple = cv2.dilate(maskPurple, None, iterations=12)
+    maskPurple = cv2.erode(maskPurple, None, iterations=erode)
+    maskPurple = cv2.dilate(maskPurple, None, iterations=dilate)
     contoursPurple, hierarchy = cv2.findContours(maskPurple,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
     contours = contoursPurple+contoursOrange+contoursBlue+contoursGreen+contoursYellow+contoursPink
@@ -88,8 +141,8 @@ def avg_size(image):
     if len(sizes)>0:
     	median=sizes[len(sizes)/2]
     	for number in sizes:
-            if (number>median-(0.6*median))and (number<median+(0.6*median)):
-	        new_sizes.append(number)
+            if (number>median*.4)and (number<median*1.2):
+	            new_sizes.append(number)
         avg_size=sum(new_sizes)/len(new_sizes)	
     else:
         avg_size=0
@@ -105,63 +158,66 @@ def find(image, avg):
     blue=0
     orange=0
     purple=0
+    low_size_range=.2
+    high_size_range=2
 
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     maskPink = cv2.inRange(hsv, pinkLower, pinkUpper)
-    maskPink = cv2.erode(maskPink, None, iterations=2)
-    maskPink = cv2.dilate(maskPink, None, iterations=12)
+    maskPink = cv2.erode(maskPink, None, iterations=erode)
+    maskPink = cv2.dilate(maskPink, None, iterations=dilate)
     contoursPink, hierarchy = cv2.findContours(maskPink,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contoursPink:
-	x,y,w,h = cv2.boundingRect(cnt)	#fit a square around the target
-	if (w*h<avg+(.40*avg))and (w*h>avg-(.40*avg)):
-	    pink=pink+1
+        x,y,w,h = cv2.boundingRect(cnt)	#fit a square around the target
+        if (w*h<avg*high_size_range)and (w*h>avg*low_size_range):
+            pink=pink+1
 
     maskYellow = cv2.inRange(hsv, yellowLower, yellowUpper)
-    maskYellow = cv2.erode(maskYellow, None, iterations=2)
-    maskYellow = cv2.dilate(maskYellow, None, iterations=12)
+    maskYellow = cv2.erode(maskYellow, None, iterations=erode)
+    maskYellow = cv2.dilate(maskYellow, None, iterations=dilate)
     contoursYellow, hierarchy = cv2.findContours(maskYellow,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contoursYellow:
-	x,y,w,h = cv2.boundingRect(cnt)	#fit a square around the target
-	if (w*h<avg+(.40*avg))and (w*h>avg-(.40*avg)):
-	    yellow=yellow+1
+        x,y,w,h = cv2.boundingRect(cnt)	#fit a square around the target
+        if (w*h<avg*high_size_range)and (w*h>avg*low_size_range):
+            yellow=yellow+1
 
     maskGreen = cv2.inRange(hsv, greenLower, greenUpper)
-    maskGreen = cv2.erode(maskGreen, None, iterations=2)
-    maskGreen = cv2.dilate(maskGreen, None, iterations=12)
+    maskGreen = cv2.erode(maskGreen, None, iterations=erode)
+    maskGreen = cv2.dilate(maskGreen, None, iterations=dilate)
     contoursGreen, hierarchy = cv2.findContours(maskGreen,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contoursGreen:
-	green=green+1
+        #x,y,w,h = cv2.boundingRect(cnt)	#fit a square around the target
+        #if (w*h<avg*high_size_range)and (w*h>avg*low_size_range):
+	    green=green+1
 
     maskBlue = cv2.inRange(hsv, blueLower, blueUpper)
-    maskBlue = cv2.erode(maskBlue, None, iterations=2)
-    maskBlue = cv2.dilate(maskBlue, None, iterations=12)
+    maskBlue = cv2.erode(maskBlue, None, iterations=erode)
+    maskBlue = cv2.dilate(maskBlue, None, iterations=dilate)
     contoursBlue, hierarchy = cv2.findContours(maskBlue,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contoursBlue:
-	x,y,w,h = cv2.boundingRect(cnt)	#fit a square around the target
-	if (w*h<avg+(.40*avg))and (w*h>avg-(.40*avg)):
-		blue=blue+1
+        x,y,w,h = cv2.boundingRect(cnt)	#fit a square around the target
+        if (w*h<avg*high_size_range)and (w*h>avg*low_size_range):
+		    blue=blue+1
 
     maskOrange = cv2.inRange(hsv, orangeLower, orangeUpper)
-    maskOrange = cv2.erode(maskOrange, None, iterations=2)
-    maskOrange = cv2.dilate(maskOrange, None, iterations=12)
+    maskOrange = cv2.erode(maskOrange, None, iterations=erode)
+    maskOrange = cv2.dilate(maskOrange, None, iterations=dilate)
     contoursOrange, hierarchy = cv2.findContours(maskOrange,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contoursOrange:
-	x,y,w,h = cv2.boundingRect(cnt)	#fit a square around the target
-	if (w*h<avg+(.40*avg))and (w*h>avg-(.40*avg)):
-            orange=orange+1
+	    x,y,w,h = cv2.boundingRect(cnt)	#fit a square around the target
+	    if ((w*h<avg*high_size_range)and(w*h>avg*low_size_range)):
+                orange=orange+1
 
     maskPurple = cv2.inRange(hsv, purpleLower, purpleUpper)
-    maskPurple = cv2.erode(maskPurple, None, iterations=2)
-    maskPurple = cv2.dilate(maskPurple, None, iterations=12)
+    maskPurple = cv2.erode(maskPurple, None, iterations=erode)
+    maskPurple = cv2.dilate(maskPurple, None, iterations=dilate)
     contoursPurple, hierarchy = cv2.findContours(maskPurple,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contoursPurple:
-	x,y,w,h = cv2.boundingRect(cnt)	#fit a square around the target
-	if (w*h<avg+(.40*avg))and (w*h>avg-(.40*avg)):
-	    purple=purple+1
-
+        x,y,w,h = cv2.boundingRect(cnt)	#fit a square around the target
+        if (w*h<avg*high_size_range)and (w*h>avg*low_size_range):
+            purple=purple+1
+        
     return [pink, yellow, green, blue, orange, purple]
 
 def count_eggs(image):
-    return find(crop_height(image),avg_size(crop_height(image)))
-
+    return find(crop_width(crop_height(image)),avg_size(crop_width(crop_height(image))))
